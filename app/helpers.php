cfd78e4d2d8f7615e64ff11b92232d530c233a58
rayhanulislamriaj2517 <riaj2517@gmail.com>
2023-09-18 22:41:03 +0600

    Updated files and added new features.

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 use App\Models\Cart;
+use App\Models\Review;
 
 function cart_amount()
 {
@@ -8,4 +9,9 @@ function cart_amount()
 function carts()
 {
     return Cart::where('user_id', auth()->id())->get();
+}
+function reviews($product_id)
+{
+    return Review::where('product_id', $product_id)->get();
+    // return $product_id;
 }
